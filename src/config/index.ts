a2fddfb94c63bf079c54249f92dd9861a44c7dc5
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2024-08-29 01:32:00 +0100

    preparations for deployment

--- src/config/index.ts
+++ src/config/index.ts
@@ -23,7 +23,9 @@ const config = {
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   GOOGLE_AUTH_CALLBACK_URL: process.env.GOOGLE_AUTH_CALLBACK_URL,
- 
+  
+
+  DATABASE_URL: process.env.DATABASE_URL,
   BASE_URL: process.env.BASE_URL,
 };
 
